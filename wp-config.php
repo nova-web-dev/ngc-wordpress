b8db35ad071a17de7d37e7b142ca6a73a787a582
Carl Eiserman <carleiserman@gmail.com>
2015-11-08 11:23:37 +0200

    Added gitignore file

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'wp-ngc');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'carleis0');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
